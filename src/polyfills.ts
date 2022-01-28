2dacf5f053f9545b0f5ba5a7ce6be4117f246a38
Ruslan Aktaev <raktaev@ronasit.com>
2022-01-29 04:52:58 +0600

    Revert "Merge branch 'raktaev/use-core-js' into 'development'"
    This reverts merge request !31

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,4 +51,4 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
- import 'core-js/proposals/reflect-metadata';
+ import 'reflect-metadata';
