1cbfd6ad7127c4ec96d152e2849527f1aa611c60
Ruslan Aktaev <raktaev@ronasit.com>
2022-02-05 05:22:58 +0600

    #15: Updated module & item facade

--- src/app/account/admin/documents/documents.module.ts
+++ src/app/account/admin/documents/documents.module.ts
@@ -4,11 +4,23 @@ import { AccountAdminDocumentsPageComponent } from './documents.component';
 import { AccountAdminDocumentsPageRoutingModule } from './documents.routing';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { ComponentStore } from '@ngrx/component-store';
 import { AccountAdminDocumentsPageFacade } from './documents.facade';
 import { AccountAdminDocumentsItemComponent } from './shared/components/item/item.component';
 import { AccountAdminDocumentsItemsComponent } from './shared/components/items/items.component';
 import { AccountAdminDocumentsItemsHeaderComponent } from './shared/components/items-header/items-header.component';
 import { AccountAdminDocumentsHeaderComponent } from './shared/components/header/header.component';
+import { DocumentModule } from '@shared/document';
+import { LoadingSpinnerModule } from '@shared/loading-spinner';
+import { HeaderSortModule } from '@shared/header-sort';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { TableContainerModule } from '@shared/table-container';
+import { NgForTrackByPropertyModule } from '@shared/ng-for-track-by-property';
+import { NotificationModule } from '@shared/notification';
+import { MediaModule } from '@shared/media';
+import { FileModule } from '@shared/file';
+// import { AccountDialogAddDocumentModule } from '@app/account/shared/dialog-add-document';
+import { DialogModule } from '@shared/dialog';
 
 @NgModule({
   declarations: [
@@ -22,10 +34,22 @@ import { AccountAdminDocumentsHeaderComponent } from './shared/components/header
     CommonModule,
     RouterModule,
     TranslateModule,
-    AccountAdminDocumentsPageRoutingModule
+    AccountAdminDocumentsPageRoutingModule,
+    DocumentModule,
+    LoadingSpinnerModule,
+    HeaderSortModule,
+    ReactiveComponentModule,
+    TableContainerModule,
+    NgForTrackByPropertyModule,
+    NotificationModule,
+    MediaModule,
+    FileModule,
+    DialogModule,
+    // AccountDialogAddDocumentModule
   ],
   providers: [
-    AccountAdminDocumentsPageFacade
+    AccountAdminDocumentsPageFacade,
+    ComponentStore
   ]
 })
 export class AccountAdminDocumentsPageModule { }
