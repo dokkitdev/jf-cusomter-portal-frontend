3a65e6249f36da9a4166c2fb4de012ae96b435cc
Ruslan Aktaev <raktaev@ronasit.com>
2022-02-09 16:40:13 +0600

    #14: Send email by default

--- src/app/account/shared/dialog-edit-user/forms/edit.ts
+++ src/app/account/shared/dialog-edit-user/forms/edit.ts
@@ -11,7 +11,7 @@ export class AccountDialogEditUserForm {
     this.name = '';
     this.email = '';
     this.roleID = UserRole.CUSTOMER;
-    this.isSendEmail = false;
+    this.isSendEmail = true;
     this.customerIDs = [];
   }
 }
