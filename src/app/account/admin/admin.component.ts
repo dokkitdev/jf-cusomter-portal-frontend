7b02116afa5540c825e497a4706a6ca9a7211512
Ruslan Aktaev <raktaev@ronasit.com>
2022-02-17 04:49:07 +0600

    #12: Removed log

--- src/app/account/admin/admin.component.ts
+++ src/app/account/admin/admin.component.ts
@@ -6,8 +6,4 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['admin.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountAdminPageComponent {
-  ngOnInit(): void {
-    console.log('fff');
-  }
-}
+export class AccountAdminPageComponent {}
