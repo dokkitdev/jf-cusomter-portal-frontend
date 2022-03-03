9b674c2aa2ccb0b30f7e1470e4e3f7b135524e14
apetrosyan <apetrosyan@ronasit.com>
2022-03-03 12:37:09 +0600

    init prod

--- src/configurations/configuration.prod.ts
+++ src/configurations/configuration.prod.ts
@@ -3,7 +3,7 @@ import { initConfiguration } from './configuration.base';
 export const configuration = initConfiguration({
   production: true,
   api: {
-    domain: 'api.jfcusomter.app',
+    domain: 'api.jamesfrewportal.co.uk/',
     get url(): string {
       return `https://${this.domain}`;
     }
