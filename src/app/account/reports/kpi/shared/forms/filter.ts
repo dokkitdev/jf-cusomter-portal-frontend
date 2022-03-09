8c91a3d92a693b0833a420f0bd499722bfd64af0
Andrei Schukin <aschukin@ronasit.com>
2022-03-09 17:04:53 +0600

    #13: Change filters

--- src/app/account/reports/kpi/shared/forms/filter.ts
+++ src/app/account/reports/kpi/shared/forms/filter.ts
@@ -3,11 +3,13 @@ export class AccountReportsKPIFilterForm {
   public archived: boolean | undefined;
   public dateCreatedFrom: string;
   public dateCreatedTo: string;
+  public isRepair: boolean;
 
   constructor() {
     this.uprn = '';
     this.archived = undefined;
     this.dateCreatedFrom = '';
     this.dateCreatedTo = '';
+    this.isRepair = true;
   }
 }
