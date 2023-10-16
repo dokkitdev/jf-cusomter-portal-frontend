ec17aa7c268db1f2206642c090b67544ec3af1c8
Ruslan Aktaev <second.roven@gmail.com>
2023-10-16 21:05:05 +0600

    feat: Added safe time filters

--- src/app/shared/job/models/pagination-request.ts
+++ src/app/shared/job/models/pagination-request.ts
@@ -79,6 +79,12 @@ export class JobPaginationRequest extends PaginationRequest {
   @Expose({ name: 'is_repair' })
   public isRepair?: boolean;
 
+  @Expose({ name: 'made_safe_date_from' })
+  public safeTimeFrom: string;
+
+  @Expose({ name: 'made_safe_date_to' })
+  public safeTimeTo: string;
+
   constructor(model: Partial<JobPaginationRequest> = {}) {
     super(model);
 
