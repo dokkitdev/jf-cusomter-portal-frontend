1c142babebe1d1be4a3ac224af68b23e88a7725a
bodyast <bodyast1996@gmail.com>
2025-08-18 12:02:21 +0300

    remove dev server add dashboard link

--- src/configurations/configuration.ts
+++ src/configurations/configuration.ts
@@ -3,7 +3,7 @@ import { initConfiguration } from './configuration.base';
 export const configuration = initConfiguration({
   production: false,
   api: {
-    domain: 'dev.api.jamesfrewportal.co.uk',
+    domain: 'api.jamesfrewportal.co.uk',
     get url(): string {
       return `https://${this.domain}`;
     }
