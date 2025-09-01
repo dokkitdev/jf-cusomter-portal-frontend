ad74912745632aa2ed09064a5cdb3b017fab0953
Ruslan <raktaev@ronasit.com>
2025-09-01 22:10:15 +0500

    feat: added mock data

--- src/app/account/reports/general/general.state.ts
+++ src/app/account/reports/general/general.state.ts
@@ -1,5 +1,21 @@
 export class AccountReportsGeneralPageState {
-  public items: any[] = [];
+  public items: any[] = [
+    {
+      date: '10/06/2025 07:26:12',
+      type: 'Asset',
+      filename: 'CHL_InstalledEquipment_Daily_202506100726.csv'
+    },
+    {
+      date: '10/06/2025 07:26:12',
+      type: 'Warehouse',
+      filename: 'CHL_Warehouse_Report_202506100726.csv'
+    },
+    {
+      date: '10/06/2025 07:26:12',
+      type: 'Zero',
+      filename: 'CHL_Zero_Report_202506100726.csv'
+    }
+  ];
   public isLoading: boolean = false;
   public sortParameters: any = {};
 
