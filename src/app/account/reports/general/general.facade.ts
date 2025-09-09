0986d759a810e36afe0a6bc3a6b4b5ec43ca5146
Ruslan <raktaev@ronasit.com>
2025-09-10 00:33:56 +0500

    lint: fix lint errors

--- src/app/account/reports/general/general.facade.ts
+++ src/app/account/reports/general/general.facade.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { switchMap, catchError } from 'rxjs/operators';
 import { tapResponse } from '@ngrx/operators';
-import { catchError } from 'rxjs/operators';
 import { NotifyService } from '@shared/notify';
 import { FileService } from '@shared/file';
 import { PaginationResponse } from '@shared/pagination';
@@ -53,6 +52,7 @@ export class AccountReportsGeneralPageFacade extends ComponentStore<AccountRepor
     origin$.pipe(
       switchMap(() => {
         const state = this.get();
+
         return this.notifyService
           .searchCsvReports({
             page: state.page,
